f8ccb3f336ae977d3a471ddb3262e779c06ec26c
VirtualPrakhar <prakhar7verma@gmail.com>
2025-05-30 12:07:06 +0530

    primitive datatype

--- 4.Javascript/part2/primitives.js
+++ 4.Javascript/part2/primitives.js
@@ -1,3 +1,22 @@
+// Number
+
 let balance =120;
+//Primitive datatype
 console.log(balance);
 console.log(typeof balance);
+
+let anotherBal = new Number(169);
+//Non-Primitive datatype
+console.log(anotherBal);
+console.log(anotherBal.valueOf());
+console.log(typeof anotherBal);
+
+// Boolean
+
+let isActive =true;
+let reallyActive = new Boolean(false);
+console.log(isActive);
+console.log(typeof (isActive));
+console.log(reallyActive);
+console.log(reallyActive.valueOf());
+console.log(typeof reallyActive);
