77683220b1c4ea324e95bdd1847a7e8353855a94
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-03 15:20:49 +0530

    primitive datatypes

--- Section10.LearnJSFoundation/part2/10.9.primitives.js
+++ Section10.LearnJSFoundation/part2/10.9.primitives.js
@@ -1,18 +1,14 @@
 // Number
-
-let balance =120;
-//Primitive datatype
+let balance =120; //Primitive datatype
 console.log(balance);
 console.log(typeof balance);
 
-let anotherBal = new Number(169);
-//Non-Primitive datatype
+let anotherBal = new Number(169); //Non-Primitive datatype cz it returns as object
 console.log(anotherBal);
 console.log(anotherBal.valueOf());
 console.log(typeof anotherBal);
 
 // Boolean
-
 let isActive =true;
 console.log(isActive);
 console.log(typeof (isActive));
@@ -23,20 +19,20 @@ console.log(reallyActive.valueOf());
 console.log(typeof reallyActive);
 
 // Null and Undefined
-
 let fname = null;
 let lname = undefined;
 console.log(fname);
 console.log(lname);
 
 // String
-
 let string1 ="hello";
 let string2 ='hola';
 let username ="Hitesh";
+// string concatenation
 let oldGreet = string1 + username + "Chaudhary" + string2;
 console.log(oldGreet);
 
+//string interpolation
 let newGreet = ` ${string1} My name is ${username} :) ${string2} `;
 console.log(newGreet);
 
