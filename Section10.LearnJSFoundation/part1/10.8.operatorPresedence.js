782417110027a9f6869905b239cb9341f6d06e17
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-03 14:37:13 +0530

    operator precedence

---
+++ Section10.LearnJSFoundation/part1/10.8.operatorPresedence.js
@@ -0,0 +1,8 @@
+//Operator Precedence
+
+let score = 2*3+2-1;
+console.log(score);
+
+//More better way because the previous way needed that you remember operator precedence table
+let score1= ((2*(3+2))-1);
+console.log(score1);
