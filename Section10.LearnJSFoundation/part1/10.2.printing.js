695ab64762a94a2749f1558818c3d11d60394656
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-03 13:34:52 +0530

    printing output in different ways

---
+++ Section10.LearnJSFoundation/part1/10.2.printing.js
@@ -0,0 +1,15 @@
+console.log("hello");
+console.log("Chai"); 
+// console.log gives output everytime on a new line
+
+// Another way of printing an output
+
+process.stdout.write("Hello Again");
+process.stdout.write("Chai "); // by this way we get output on the same line
+
+console.warn("Stop Here");
+
+console.table({city: "Jaipur"});
+
+// console.clear(); 
+// used for to clear the console
