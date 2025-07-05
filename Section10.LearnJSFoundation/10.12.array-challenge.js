98330e9230eb1bbf5f9eaf7f35788043150ddfa1
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-05 20:53:50 +0530

    array challenges

---
+++ Section10.LearnJSFoundation/10.12.array-challenge.js
@@ -0,0 +1,49 @@
+/* Challenge 1: Declare an array named 'teaFlavors' that contains the strings "green tea","black tea","oolong tea". 
+Access the first element of the array and store it in a variable named "firstTea". */
+
+
+
+/* Challenge 2: Declare an array named 'cities' containing "London","Tokyo","Paris","NewYork". 
+Access the third element in the array and store it in a variable named 'favoriteCity'. */
+
+
+
+
+/* Challenge 3: You have an array named 'teaTypes' containing "Herbal Tea","white Tea","masala tea".
+Change the second element of the array to "jasmine tea".  */
+
+
+
+
+/* Challenge 4: Declare an array named 'citiesVisited' containing "mumbai","sydney".
+Add "berlin" to the array using push method. */
+
+
+
+/* Challenge 5: You have an array named 'teaOrders' with "chai","iced tea","matcha","earl grey".
+Remove the last element of the array using the pop method and store it in a variable named 'lastOrder'.   */
+
+
+
+/* Challenge 6: You've an array named 'popularTeas' containing "green tea","oolong tea","chai".
+Create a soft copy of this array named 'softCopyTeas'.  */
+
+
+
+/* Challenge 7: You have an array named 'topCities' containing "Berlin","singapore","new york".
+Create a hard copy of this array named "hardCopyCities".  */
+
+
+
+/* Challenge 8: You have two arrays, 'european cities' containing "paris","rome" and 'Asian Cities' containing "tokyo","bangkok".
+Merge these two arrays into a new array named 'worldCities'. */
+
+
+
+/* Challenge 9: You have an array named 'teaMenu' containing "masala Chai","oolong tea", "green tea","earl grey".
+Find the length of the array and store it in a variable named 'menuLength'.  */
+
+
+
+/* Challenge 10: You have an array named 'cityBucketList' containing "kyoto","london","cape town","vancouver".
+Check if "london" is in the array and store the result in the variable named 'isLondonInList' */
