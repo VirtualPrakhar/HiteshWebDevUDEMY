1bd34e57c801c11972f31fba453b5862af6d992a
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-05 17:39:51 +0530

    conditional statements challenge 1

---
+++ Section10.LearnJSFoundation/10.11.conditionals-challenge.js
@@ -0,0 +1,16 @@
+// Challenge 1 :Checking if a number is greater than another number
+
+
+
+// Challenge 2 :Check if a string is equal to another string
+
+
+// Challenge 3 :Checking if a variable is number or not
+
+
+// Challenge 4 :Checking if a boolean value is true or false
+
+
+// Challenge 5 :Checking if an array is empty or not
+
+
