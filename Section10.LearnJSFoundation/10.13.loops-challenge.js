ab8fee150406d23cff20bc480b3332a579f7612b
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-06 20:46:40 +0530

    basic loop challenges questions done

---
+++ Section10.LearnJSFoundation/10.13.loops-challenge.js
@@ -0,0 +1,31 @@
+// Six Loop Challenges
+
+
+
+/* Challenge 1: Write a while loop that calculates the sum of all numbers from 1 to 5 and store the result in a variable named 'sum'. */
+
+
+console.log("#############################");
+
+
+/* Challenge 2: Write a while loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown'. */
+
+
+console.log("#############################");
+
+/* Challenge 3: Write a do while loop that prompts a user to enter their favorite tea type until they enter "stop". Store each tea type in an array named 'teaCollection'. */
+
+
+console.log("#############################");
+
+/* Challenge 4: Write a do while loop that adds numbers from 1 to 3 and stores the result in a variable named `total`. */
+
+
+console.log("#############################");
+
+/* Challenge 5: Write a for loop that multiplies each element in the array [2, 4, 6] by 2 and stores the results in a new array named 'multipliedNumbers'. */
+
+
+console.log("#############################");
+
+/* Challenge 6: Write a for loop that lists all the cities in the array ["Paris", "New York", "Tokyo", "London"]` and stores each city in a new array named 'cityList`. */
