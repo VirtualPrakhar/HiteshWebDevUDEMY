3432ae617b7bb77e3f1e8c08992af1e7e8af43fa
VirtualPrakhar <prakhar7verma@gmail.com>
2025-07-18 10:51:09 +0530

    fundtion in js

---
+++ Section10.LearnJSFoundation/10.15.functions.js
@@ -0,0 +1,33 @@
+/* Challenge 1: Write a function named 'makeTea` that takes one parameter, typeofTea', and returns a string like "Making green tea" when called with "green tea"`. Store the result in a variable named 'teaOrder'. */
+
+
+
+console.log("########################");
+
+/* Challenge 2: Create a function named 'orderTea that takes one parameter, `teaType`. Inside this function, create another function named 'confirmorder that returns a message like "Order confirmed for chai".
+Call confirmOrder from within orderTea and return the result. */
+
+
+
+console.log("########################");
+
+/* Challenge 3: Write an arrow function named calculateTotal that takes two parameters: `price` and `quantity. The function should return the total cost by multiplying the `price` and `quantity`.
+Store the result in a variable named 'totalCost`. */
+
+
+
+console.log("########################");
+
+/* Challenge 4: Write a function named processTeaOrder` that takes another function, `makeTea, as a parameter and calls it with the argument "earl grey".
+Return the result of calling makeTea`.  */
+
+
+
+console.log("########################");
+
+/* Challenge 5: Write a function named createTeaMaker that returns another function. The returned function should take one parameter, `teaType`, and return a message like "Making green tea",
+Store the returned function in a variable named teaMaker and call it with "green tea". */
+
+
+
+console.log("########################");
