4fd621e888baddef8fd46801bc77be165ff8de85
VirtualPrakhar <prakhar7verma@gmail.com>
2025-08-27 13:49:18 +0530

    summary of prototype in js

--- Section10.LearnJSFoundation/10.16.prototypes.js
+++ Section10.LearnJSFoundation/10.16.prototypes.js
@@ -39,3 +39,26 @@ console.log(obj1.hasOwnProperty(`property1`));
 console.log(obj1.hasOwnProperty(`toString`));
 
 console.log(obj1.hasOwnProperty(`hasOwnProperty`));
+
+
+
+
+/* 
+Summarize====================>>>>>>>>>>>>>>>>>>>>>>
+
+This lecture provides a comprehensive overview of prototypes in JavaScript, transitioning from basic concepts to more advanced programming paradigms. Here are the main points:
+
+Foundation Review: The lecture begins with a review of essential JavaScript concepts, encouraging practice through coding exercises.
+
+Prototype-Based Language: JavaScript is emphasized as a prototype-based language, which allows inheritance and property sharing among objects.
+
+Object-Oriented Programming (OOP): Key concepts such as classes, constructors, and instances are introduced, explaining that classes act as blueprints for creating objects.
+
+Practical Examples: The instructor demonstrates practical applications, like creating forms and grouping input fields into a class, using diagrams to illustrate class-object relationships.
+
+Manipulating Prototypes: An in-depth explanation of how prototypes work, including access and manipulation using the __proto__ property and the Object.getPrototypeOf() method.
+
+Understanding Properties: The hasOwnProperty() method is discussed, explaining its importance in distinguishing between an object’s own properties and those inherited from its prototype.
+
+Encouragement for Exploration: The lecture concludes by encouraging further exploration of JavaScript's prototype system, setting the stage for advanced topics in upcoming lectures. 
+*/
