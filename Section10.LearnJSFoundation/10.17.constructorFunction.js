d5cfa12a6cdb3417de82e5dd9c9ac54e62bf51e9
VirtualPrakhar <prakhar7verma@gmail.com>
2025-08-30 13:18:21 +0530

    constructor functions and also constructor func with prototypes in js

---
+++ Section10.LearnJSFoundation/10.17.constructorFunction.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+/* 
+Summarize ===============>>>>>>>>>>>>>>
+
+This lecture focuses on functional constructors in JavaScript and how they differ from traditional object-oriented programming. Here are the main points:
+
+Understanding Functional Programming: Importance of grasping functional programming principles before diving into constructor syntax.
+
+Constructor Function: Introduces constructor functions as blueprints for creating multiple objects with shared properties and methods.
+
+Best Practices: Emphasizes the use of the 'function' keyword and the convention of capitalizing the first letter of the function name.
+
+Using 'this' Keyword: Explains how 'this' refers to the newly created object within a constructor function.
+
+Object Instantiation: Covers how to instantiate objects using the 'new' keyword to create instances while retaining their unique properties.
+
+Prototypes: Discusses how prototypes can be used to add methods to constructor functions, with an example involving an 'Animal' constructor.
+
+Error Handling: Introduces methods to check if a constructor function is called without 'new' and how to throw errors for proper usage.
+
+Overall, it provides a comprehensive overview of functional constructors, covering key concepts such as constructor functions, the 'this' keyword, object instantiation, prototypes, and error handling. 
+*/
