eb2cd998ab2734600080e2a2822ff63741d5a76d
VirtualPrakhar <prakhar7verma@gmail.com>
2025-09-07 20:29:22 +0530

    constructor function in js

--- Section10.LearnJSFoundation/10.17.constructorFunction.js
+++ Section10.LearnJSFoundation/10.17.constructorFunction.js
@@ -1,3 +1,74 @@
+// Normal Function
+function radha(){
+    console.log("Radhe Shyam");
+}
+radha();
+
+// Constructor Function
+function Person(name){
+    console.log("Radha",name);
+}
+Person("Krishn");
+
+
+function Person2(name,age){
+    this.name = name;
+    this.age = age;
+    console.log(name,age);
+}
+Person2("RadhaRani",21);
+
+
+function Car(make,model){
+    this.make = make;
+    this.model = model;
+}
+let myCar = new Car("Mahindra","Bolero");
+console.log(myCar);
+let myNewCar = new Car("Kia","Seltos");
+console.log(myNewCar);
+
+//without 'new' keyword
+function Car2(make,model){
+    this.make = make;
+    this.model = model;
+}
+let myCar2 = Car("Tata","Safari");
+console.log(myCar2);
+
+
+//we can declare new values too by the help of 'this' keyword
+function Tea(type){
+    this.type = type;
+    //declaring new variable(paramerter)
+    this.describe = function(){
+        return `That is a Cup of ${this.type}`;
+    }
+}
+let lemonTea = new Tea("Lemon Tea");
+console.log(lemonTea);
+console.log(lemonTea.describe());
+
+// Constructor Function with Prototype
+function Animal(species){
+    this.species = species;
+}
+Animal.prototype.sound = function(){
+    return `${this.species} makes noise`;
+}
+let dog = new Animal("Dog");
+console.log(dog.sound());
+
+// Some Common Errors
+function Drink(name){
+    if(!new.target){
+        throw new Error("Drink must be called with new keyword");
+    }
+    this.name = name;
+}
+let tea = new Drink("Tea");
+let coffee = Drink("Coffee"); //it will throw us error cz we made a check to find it
+
 
 
 
